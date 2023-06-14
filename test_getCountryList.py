70b93b51c5fb2512effb9691fa2b8c5e5b29663c
Auntor Acharja <acharjeeauntor@gmail.com>
2023-06-14 08:08:15 +0600

    Added Script

--- test_getCountryList.py
+++ test_getCountryList.py
@@ -1,19 +1,38 @@
-from playwright.sync_api import Page,expect
+from playwright.sync_api import Page
+import csv
 
 def test_save_country_name(page:Page):
     country_section_selector = "#nav div"
     country_lists_selector = "ul li a"
     country_title_selector = "#content h1"
+    sub_country_list_selector = "tbody tr"
+    sub_country_data_selector = "td"
 
 
     page.goto("/")
     country_section = page.locator(country_section_selector).nth(3)
     country_lists = country_section.locator(country_lists_selector)
-    totalCountry = country_lists.count()
-    for singleCountry in range(totalCountry):
-        country_name_option = country_lists.nth(singleCountry)
+    total_country = country_lists.count()
+    for single_country_index in range(total_country):
+        country_name_option = country_lists.nth(single_country_index)
         country_name = country_name_option.inner_text()
-        print(country_name)
+        #print(country_name)
         country_name_option.click()
         country_title = page.locator(country_title_selector).nth(0).inner_text()
-        print(country_title)
+        #print(country_title)
+        sub_country_list = page.locator(sub_country_list_selector)
+        total_sub_country = sub_country_list.count()
+        for sub_country_index in range(1,total_sub_country):
+            sub_country_name = sub_country_list.nth(sub_country_index).locator(sub_country_data_selector).nth(1).inner_text()
+            #print(sub_country_name)
+            
+            # Store Data into CSV File ---- Start From Here ----
+            # Header
+            header = ['Country Name']
+            # File path to save the CSV
+            file_path = f'{country_name}.csv'
+            with open(file_path, 'w', newline='') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow(header)
+                csv_writer.writerow([sub_country_name])
+            print('Data stored in CSV successfully.')
